3b3d68b8837cd79156733446863c5de52d1d9cab
Yongjoon Kwon <littlesnail0513@gmail.com>
2026-04-03 13:24:38 +0900

    [feature] 46p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -29,5 +29,6 @@ class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
+        assertIllegalArgument("12s");
     }
 }
